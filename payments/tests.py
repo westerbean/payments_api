77af4a60567c44c65210216e6192a8389120eb00
westerbean <femiaderewa@icloud.com>
2025-05-20 11:29:40 +0100

    first commit

--- payments/tests.py
+++ payments/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
 
-# Create your tests here.
+class PaymentAPITest(TestCase):
+    def test_payment(self):
+        client = APIClient()
+        data = {
+            "name": "Jane Doe",
+            "email": "jane@example.com",
+            "amount": "1000",
+            "gateway": "paystack"
+        }
+        response = client.post("/api/v1/payment/", data, format='json')
+        self.assertEqual(response.status_code, 200)
