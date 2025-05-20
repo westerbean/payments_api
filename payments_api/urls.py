54018348694e061d040a1045fff1b39be49bb1b7
westerbean <femiaderewa@icloud.com>
2025-05-20 12:01:17 +0100

    Added a root path

--- payments_api/urls.py
+++ payments_api/urls.py
@@ -1,7 +1,12 @@
+from django.http import JsonResponse
 from django.contrib import admin
 from django.urls import path, include
 
+def root_view(request):
+    return JsonResponse({"message": "Payments API is running."})
+
 urlpatterns = [
+    path('', root_view),
     path('admin/', admin.site.urls),
     path('api/v1/', include('payments.urls')),
 ]
